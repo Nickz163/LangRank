b9a30b7abc53c2a5db6ae2365e532e665dc72fa4
Your Name <your@email.address>
2019-08-09 20:51:30 +0300

    Mongo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,36 +1,39 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-public class App {
-
-    public static void main(String[] args) throws IOException {
-
-     TiobeRatingParser tiobeParser = TiobeRatingParser.getInstance();
-     //tiobeParser.parseWholeData();
-        List<String> languagesNames = tiobeParser.parseWholeData().stream()
-                .map(lang->lang.getName())
-                .collect(Collectors.toList());
-
-//        languagesNames.forEach(a->System.out.println(a));
-
-
-//        PYPLRatingParser pyplRatingParser = PYPLRatingParser.getInstance();
-//        pyplRatingParser.parseWholeData();
-//        pyplRatingParser.saveDataInPlainFormat();
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
 //
-        SOvFParser sOvFParser = SOvFParser.getInstance(languagesNames);
-       // sOvFParser.parseData("java");
-        sOvFParser.parseWholeData();
-        //sOvFParser.saveDataInPlainFormat();
-    }
-}
+//import org.jsoup.select.Elements;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.function.Function;
+//import java.util.stream.Collectors;
+//
+//public class App {
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        app.TiobeRatingParser tiobeParser = app.TiobeRatingParser.getInstance();
+//        //tiobeParser.parseWholeData();
+//        List<String> languagesNames = tiobeParser.parseWholeData().stream()
+//                .map(lang -> lang.getName())
+//                .collect(Collectors.toList());
+//
+////       languagesNames.forEach(a->System.out.println(a));
+//
+//
+//        app.SOvFParser sOvFParser = app.SOvFParser.getInstance(languagesNames);
+//        // sOvFParser.parseData("java");
+//        List<app.LanguageDataPrototype> langSOvF = sOvFParser.parseWholeData();
+////        langSOvF.forEach(a -> a.printLang());
+//
+////        sOvFParser.saveDataInPlainFormat();
+//
+//
+//        app.PYPLRatingParser pyplRatingParser = app.PYPLRatingParser.getInstance();
+//        pyplRatingParser.parseWholeData();
+//        // pyplRatingParser.saveDataInPlainFormat();
+//    }
+//}
